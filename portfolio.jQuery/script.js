ccd4186a47ca7ce93294b39da2803f6dc51552ff
maloha <yume6396@gmail.com>
2023-07-18 20:51:10 +0900

    初回コミット

---
+++ portfolio.jQuery/script.js
@@ -0,0 +1,10 @@
+$(".carousel").slick({
+  autoplay: true,
+  dots: true,
+  infinite: true,
+  autoplaySpeed: 5000,
+  arrows: false,
+  centerMode: true,
+  centerPadding: "5%",
+  arrows: true,
+});
